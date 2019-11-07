7f1cdb9504da19f31e29146cf2434b4ab8645357
Amer Mamoun <amer.mn.96@hotmail.com>
2019-11-07 17:30:33 +0100

    Application class

---
+++ demo/src/Application.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class Application extends Component {
+     
+    constructor(props){
+        super(props);
+    }
+
+    componentWillMount(props, state){
+
+    }
+    componentDidMount(props, state){
+        console.log("Mounted with", props, state);
+    }
+    componentWillReceiveProps(props){
+
+    }
+
+    componentWillUpdate(props, state){
+        if(this.props.name !== props.name){
+            //do sth
+        }
+    }
+    componentDidUpdate(props, state){
+              
+    }
+
+
+    render(){
+        let name = "Amer";
+        
+        return (
+            <div>
+                <h1>Hello, {name}</h1>
+                <span>This</span>
+            </div>
+        );
+    }
+}
+
+export default Application;
